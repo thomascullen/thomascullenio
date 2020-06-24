c9397bc66aa0537752eec4c1755c3b912ec6f187
Thomas Cullen <thomascullen92@gmail.com>
2020-06-24 19:31:45 +0100

    Export styled components

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,7 +1,27 @@
 import React from "react"
 
+import styled from "styled-components"
+
+const StyledComponent = styled.div``
+
+function SubComponent({ children }) {
+  return <StyledComponent>{children}</StyledComponent>
+}
+
+SubComponent.toString = () => StyledComponent.toString()
+
+const ParentStyles = styled.div`
+  ${SubComponent} {
+    background: red;
+  }
+`
+
 function Playground() {
-  return <div>playground</div>
+  return (
+    <ParentStyles>
+      <SubComponent>asdf</SubComponent>
+    </ParentStyles>
+  )
 }
 
 export default Playground
